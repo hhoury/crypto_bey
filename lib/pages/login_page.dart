84c767122d08bfa36f16a0b9058a12f2da9a8573
hhoury <hhoury@hotmail.com>
2022-04-13 21:22:24 +0300

    .

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,41 +1,73 @@
 // ignore_for_file: use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class LoginPage extends StatelessWidget {
   static const routeName = '/login';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      margin: const EdgeInsets.all(15),
-      child: Column(
-        children: [
-          ClipRRect(
-            child: Image.asset(
-              'assets/images/logo.png',
-              fit: BoxFit.cover,
-              width: 220,
-              height: 200,
+        body: SingleChildScrollView(
+      child: Container(
+        margin: const EdgeInsets.all(15),
+        child: Column(
+          children: [
+            ClipRRect(
+              child: SvgPicture.asset('assets/images/logo.svg',
+                  alignment: Alignment.topCenter),
             ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          const Align(alignment: Alignment.centerLeft, child: Text('Email')),
-          const TextField(),
-          const Align(alignment: Alignment.centerLeft, child: Text('Password')),
-          const TextField(),
-          Align(
-              alignment: Alignment.centerRight,
-              child: TextButton(
-                  onPressed: () {}, child: const Text('Forgot Password'))),
-          const SizedBox(
-            height: 50,
-          ),
-          ElevatedButton(onPressed: () {}, child: const Text('LOGIN')),
-          ElevatedButton(onPressed: () {}, child: const Text('SIGNUP')),
-        ],
+            const Align(alignment: Alignment.centerLeft, child: Text('Email')),
+            const TextField(),
+            const Align(
+                alignment: Alignment.centerLeft, child: Text('Password')),
+            const TextField(),
+            Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                    onPressed: () {}, child: const Text('Forgot Password'))),
+            const SizedBox(
+              height: 50,
+            ),
+            FractionallySizedBox(
+              widthFactor: 0.8,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                  child: Text('LOGIN'),
+                ),
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            FractionallySizedBox(
+              widthFactor: 0.8,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                  child: Text('SIGN UP'),
+                ),
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     ));
   }
